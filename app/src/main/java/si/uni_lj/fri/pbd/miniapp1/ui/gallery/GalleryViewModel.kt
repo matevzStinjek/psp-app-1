5d0355ffda12b7bbc5184c1db96a69ff07c45d6e
matevzStinjek <stinjek@gmail.com>
2021-03-03 10:17:05 +0100

    init

---
+++ app/src/main/java/si/uni_lj/fri/pbd/miniapp1/ui/gallery/GalleryViewModel.kt
@@ -0,0 +1,13 @@
+package si.uni_lj.fri.pbd.miniapp1.ui.gallery
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class GalleryViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is gallery Fragment"
+    }
+    val text: LiveData<String> = _text
+}
